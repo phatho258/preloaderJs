bd1c9ea9bf42b1ee82cbc9499e65761241297a4e
phatho258 <158005788+phatho258@users.noreply.github.com>
2024-03-20 15:29:00 +0700

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -8,8 +8,8 @@ overlay.style.cssText = `
     position: fixed;
     top: 0;
     left: 0;
-    width: 100%;
-    height: 100%;
+    width: 200px;
+    height: 200px;
     background-color: rgba(255, 255, 255, 0.7);
     z-index: 999;
 `;

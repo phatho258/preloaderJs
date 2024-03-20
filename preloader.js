48cfa27ab77ecf6bf8c6eb448e9fcd98252959bb
phatho258 <158005788+phatho258@users.noreply.github.com>
2024-03-20 14:49:55 +0700

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,7 +16,7 @@ overlay.style.cssText = `
 
 const svgImage = document.createElement('img');
 svgImage.id = 'svgImage';
-svgImage.src = 'https://jvsc99.github.io/preloaderJs/imagem.svg';
+svgImage.src = 'https://phatho258.github.io/preloaderJs/product-launch.svg';
 svgImage.style.cssText = `
     max-width: 100%;
     max-height: 100%;

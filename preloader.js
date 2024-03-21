c67dc0a8804a67ef40c2de30586243487acfef94
phatho258 <158005788+phatho258@users.noreply.github.com>
2024-03-21 11:58:09 +0700

    Update preloader.js

--- preloader.js
+++ preloader.js
@@ -16,8 +16,8 @@ overlay.style.cssText = `
 const lottieContainer = document.createElement('div');
 lottieContainer.id = 'lottieContainer';
 lottieContainer.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
+    width: 200px;
+    height: 200px;
     display: none;
     position: absolute;
     top: 50%;

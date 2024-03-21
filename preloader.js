cf3b1eb5f032d64f374381754e81d7f92c69fa72
phatho258 <158005788+phatho258@users.noreply.github.com>
2024-03-21 11:57:06 +0700

    Update preloader.js using lottie

--- preloader.js
+++ preloader.js
@@ -1,4 +1,3 @@
-// Create the overlay and SVG elements
 const overlay = document.createElement('div');
 overlay.id = 'loader';
 overlay.style.cssText = `
@@ -14,37 +13,37 @@ overlay.style.cssText = `
     z-index: 999;
 `;
 
-const svgImage = document.createElement('img');
-svgImage.id = 'svgImage';
-svgImage.src = 'https://phatho258.github.io/preloaderJs/product-launch.svg';
-svgImage.style.cssText = `
-    width: 200px;
-    height: 200px;
+const lottieContainer = document.createElement('div');
+lottieContainer.id = 'lottieContainer';
+lottieContainer.style.cssText = `
+    max-width: 100%;
+    max-height: 100%;
     display: none;
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
 `;
-svgImage.style.display = 'none';
+lottieContainer.style.display = 'none';
 
-// Append the elements to the body
 document.body.appendChild(overlay);
-document.body.appendChild(svgImage);
+document.body.appendChild(lottieContainer);
 
-// Function to hide the overlay and display the SVG
 function hideOverlay() {
     overlay.style.display = 'none';
-    svgImage.style.display = 'block';
+    lottieContainer.style.display = 'block';
+    // Substitua pela URL direta do seu arquivo JSON do Lottie.
+    lottie.loadAnimation({
+        container: lottieContainer,
+        renderer: 'svg', // ou 'canvas' se preferir
+        loop: true,
+        autoplay: true,
+        path: 'https://phatho.github.io/preloaderJs/preload-lottie.json',
+    });
 }
 
-// Add an event listener to hide the overlay when all external JS files are loaded
 document.addEventListener('DOMContentLoaded', () => {
-    // Replace the following lines with the actual code that loads your external JS files
-    // For demonstration purposes, we'll use a setTimeout to simulate loading external JS files.
-    hideOverlay();
+    hideOverlay(); // Teste sem o setTimeout
 });
 
-// Fallback: If all external resources are loaded and the DOMContentLoaded event doesn't fire,
-// we'll still hide the overlay when the window's load event is triggered.
 window.addEventListener('load', hideOverlay);
